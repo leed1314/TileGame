1ab1c523dd752a9497fb9195e79f5a3e32683c11
liuchen <937038776@qq.com>
2020-01-07 17:28:35 +0800

    tilemap 初步

---
+++ assets/tiles_sheet.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="tiles_sheet" tilewidth="64" tileheight="64" tilecount="96" columns="16">
+ <image source="tiles_sheet.png" width="1024" height="384"/>
+ <tile id="17">
+  <properties>
+   <property name="isSailable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="72">
+  <properties>
+   <property name="isSailable" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
